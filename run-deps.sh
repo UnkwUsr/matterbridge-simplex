86b01d1912b798fabb615fb6041dcb9eb06fad36
UnkwUsr <ktoto2707043@gmail.com>
2024-08-19 19:39:39 +0300

    upd run-deps

--- run-deps.sh
+++ run-deps.sh
@@ -1,8 +1,10 @@
 tmux split-window -h 'simplex-chat -p 5225'
 tmux split-window -h 'sudo ~/Downloads/ssh-chat/ssh-chat --verbose --bind 127.0.0.1:48320 --identity /home/meda/.ssh/id_rsa'
-# tmux split-window -h ''
 
 echo 'waiting 5 sec, you starting ssh-chat' && sleep 5
+tmux split-window -h 'ssh 127.0.0.1 -p 48320'
 matterbridge -conf matter-simplex.toml
+
+tmux select-layout even-horizontal
 
 # node main.js
