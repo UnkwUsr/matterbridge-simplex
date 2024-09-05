a915b6086e485c43b080d57fe776bebfae8b2ff2
UnkwUsr <ktoto2707043@gmail.com>
2024-09-05 07:21:36 +0300

    test: chore

--- run-deps.sh
+++ run-deps.sh
@@ -1,5 +1,5 @@
 tmux split-window -h 'simplex-chat -p 5225'
-tmux split-window -h 'sudo ~/Downloads/ssh-chat/ssh-chat --verbose --bind 127.0.0.1:48320 --identity /home/meda/.ssh/id_rsa'
+tmux split-window -h 'sudo ./ssh-chat/ssh-chat --verbose --bind 127.0.0.1:48320 --identity /home/meda/.ssh/id_rsa'
 
 echo 'waiting 5 sec, you starting ssh-chat' && sleep 5
 tmux split-window -h 'ssh 127.0.0.1 -p 48320'
